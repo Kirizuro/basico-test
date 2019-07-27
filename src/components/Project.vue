c4b9d7d3b1856a891e5a04a7c3d685d444a0a998
Claudio <claudio.junior14@hotmail.com>
2019-07-26 21:30:19 -0300

    Last fixes

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -9,6 +9,13 @@
           para edição que permite o dono adicionar, remover ou alterar qualquer um dos seguros assim como
           suas categorias e também possui um blog integrado.
         </p>
+        <h1 class="text-uppercase font-weight-light ma-10">Porque quero trabalhar na Basico.com!</h1>
+        <p>
+          O maior motivo para o meu desejo em trabalhar na basico.com é para poder me aperfeiçoar como profissional
+          assim como sei que poderei ser de ajuda para o crescimento da empresa, estou muito motivado para poder
+          desempenhar minha função em prol da empresa e também implementar novas features para que os clientes atuais
+          da basico possam respeitar ainda mais a empresa.
+        </p>
       </v-flex>
     </v-layout>
   </v-container>
